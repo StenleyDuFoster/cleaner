9b457727cdf44a7261ebc609ff36ee3d7445e107
Andrew <rembozebest@gmail.com>
2021-10-20 16:34:42 +0300

    Minor changes

--- app/src/main/java/com/stenleone/clenner/notification/Constants.kt
+++ app/src/main/java/com/stenleone/clenner/notification/Constants.kt
@@ -2,3 +2,5 @@ package com.stenleone.clenner.notification
 
 const val SEND_PUSH_ACTION = "PUSH"
 const val NOTIFY_STORAGE = "NOTIFY_PRESENT"
+const val NOTIFY_PERIOD_H = 2
+const val NOTIFY_PERIOD_M = 0
